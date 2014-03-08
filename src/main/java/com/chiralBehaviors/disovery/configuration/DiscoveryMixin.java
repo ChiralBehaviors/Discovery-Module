2978a190401f84a4a01e122db843e23838a36581
Hellblazer <hellblazer@me.com>
2014-03-08 12:17:17 -0800

    Frist

---
+++ src/main/java/com/chiralBehaviors/disovery/configuration/DiscoveryMixin.java
@@ -0,0 +1,38 @@
+package com.chiralBehaviors.disovery.configuration;
+
+/*
+ * (C) Copyright 2014 Chiral Behaviors, All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+ 
+
+import com.chiralBehaviors.slp.hive.configuration.HiveScopeConfiguration;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.hellblazer.nexus.config.GossipScopeConfiguration;
+import com.hellblazer.slp.local.config.LocalScopeConfiguration;
+
+/**
+ * @author hhildebrand
+ *
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+               @Type(value = HiveScopeConfiguration.class, name = "hiveScope"),
+               @Type(value = GossipScopeConfiguration.class, name = "gossipScope"),
+               @Type(value = LocalScopeConfiguration.class, name = "localScope") })
+public class DiscoveryMixin {
+
+}
---
+++ src/main/java/com/chiralBehaviors/disovery/configuration/DiscoveryModule.java
@@ -0,0 +1,55 @@
+/** 
+ * (C) Copyright 2014 Chiral Behaviors, All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.chiralBehaviors.disovery.configuration;
+
+import java.net.InetSocketAddress;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.hellblazer.nexus.config.GossipScopeConfiguration;
+import com.hellblazer.slp.config.ServiceScopeConfiguration;
+import com.hellblazer.utils.deserializers.InetSocketAddressDeserializer;
+import com.hellblazer.utils.fd.FailureDetectorFactory;
+import com.hellblazer.utils.fd.json.FdFactoryMixin;
+
+/**
+ * @author hhildebrand
+ * 
+ */
+public class DiscoveryModule extends SimpleModule {
+
+    private static final long serialVersionUID = 1L;
+
+    public DiscoveryModule() {
+        super("Discovery Module");
+    }
+
+    /* (non-Javadoc)
+     * @see com.fasterxml.jackson.databind.module.SimpleModule#setupModule(com.fasterxml.jackson.databind.Module.SetupContext)
+     */
+    @Override
+    public void setupModule(SetupContext context) {
+        super.setupModule(context);
+        setMixInAnnotation(ServiceScopeConfiguration.class,
+                           GossipScopeConfiguration.class);
+        super.setupModule(context);
+        setMixInAnnotation(ServiceScopeConfiguration.class,
+                           DiscoveryMixin.class);
+        setMixInAnnotation(FailureDetectorFactory.class, FdFactoryMixin.class);
+        addDeserializer(InetSocketAddress.class,
+                        new InetSocketAddressDeserializer());
+    }
+
+}
